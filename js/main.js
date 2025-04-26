b720a68a7de8d68b5fd2afbf5e879373f37bbab4
artt1c <roma.myalo000@gmail.com>
2025-04-26 15:16:23 +0300

    Header UI done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import ApiService from './services/api.service.js';
 
 console.log(await ApiService.product.getAll())
+console.log(await ApiService.product.getSingle(1))
 console.log(await ApiService.product.getCategoriesList())
 console.log(await ApiService.product.getCategory('laptops'))
--- js/services/api.service.js
+++ js/services/api.service.js
@@ -10,6 +10,21 @@ const ApiService = {
       try {
         const response = await fetch(baseUrl + urls.products.basePath)
 
+        if(!response.ok) throw new Error(`Response status: ${response.status}`)
+
+        return response.json();
+      } catch (e) {
+        console.log(e)
+      }
+    },
+
+    getSingle: async (id) => {
+      if (!id) console.error('Id can`t be empty')
+      try {
+        const response = await fetch(baseUrl + urls.products.basePath + `/${id}`)
+
+        if(!response.ok) throw new Error(`Response status: ${response.status}`)
+
         return response.json();
       } catch (e) {
         console.log(e)
@@ -19,6 +34,8 @@ const ApiService = {
     getCategoriesList: async () => {
       try {
         const response = await fetch(baseUrl + urls.products.categoriesList)
+
+        if(!response.ok) throw new Error(`Response status: ${response.status}`)
 
         return response.json();
       } catch (e) {
@@ -30,6 +47,8 @@ const ApiService = {
       if (!categoryName) console.error('Category can`t be empty')
       try {
         const response = await fetch(baseUrl + urls.products.category + `/${categoryName}`)
+
+        if(!response.ok) throw new Error(`Response status: ${response.status}`)
 
         return response.json();
       } catch (e) {
