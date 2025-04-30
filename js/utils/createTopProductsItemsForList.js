9f6cc9cf22b7b91526125115f69138dc84592e6b
artt1c <roma.myalo000@gmail.com>
2025-05-01 01:18:57 +0300

    Top product UI

--- js/utils/createTopProductsItemsForList.js
+++ js/utils/createTopProductsItemsForList.js
@@ -25,32 +25,63 @@ const createProductsItemsForList = async () => {
     const productSlideTitle = document.createElement('h4')
     productSlideTitle.textContent = product.title
 
+    // FEEDBACK SECTION
+    const feedbackSectionLink = document.createElement('a')
+    feedbackSectionLink.href = '#';
+    feedbackSectionLink.classList.add('slide__feedback-link')
+
+    const feedbackSection = document.createElement('div')
+    feedbackSection.classList.add('slide__feedback')
+
     // RATING
     const productSlideRating = document.createElement('p')
     productSlideRating.textContent = product.rating
+    productSlideRating.classList.add('slide__rating')
 
     // REVIEWS
     const productSlideReviews = document.createElement('p')
     productSlideReviews.textContent = product.reviews.length
+    productSlideReviews.classList.add('slide__reviews')
+
+    feedbackSection.append(productSlideRating, productSlideReviews)
+    feedbackSectionLink.appendChild(feedbackSection)
+
+    // BUY GROUP
+    const buyGroup = document.createElement('div')
+    buyGroup.classList.add('slide__buy-group')
 
     // PRICE
     const productSlidePrice = document.createElement('p')
-    productSlidePrice.textContent = product.price + ' $'
+    productSlidePrice.textContent = product.price
+    productSlidePrice.classList.add('slide__price')
+    productSlidePrice.innerHTML += '<span class="price-currency">$</span>'
+
+    // PRODUCT BUY CONTROL
+    const productControl = document.createElement('div')
+    productControl.classList.add('slide__buy-control')
+
+    const scalesBtn = document.createElement('button')
+    scalesBtn.classList.add('buy-control__btn-scales')
+
+    const cartBtn = document.createElement('button')
+    cartBtn.classList.add('buy-control__btn-cart')
+
+    productControl.append(scalesBtn, cartBtn)
+    buyGroup.append(productSlidePrice, productControl)
     
 
     productSlide.append(
       swiperBlock,
       productSlideTitle,
-      productSlideRating,
-      productSlideReviews,
-      productSlidePrice
+      feedbackSectionLink,
+      buyGroup,
     )
     wrapper.appendChild(productSlide)
 
 
     // IMAGE SWIPER INIT
     if (swiperId) {
-      new Swiper('#' + swiperId.id, {
+      const imgSwiper = new Swiper('#' + swiperId.id, {
         slidesPerView: 1,
         spaceBetween: 0,
         loop: true,
@@ -59,13 +90,46 @@ const createProductsItemsForList = async () => {
           nextEl: '#' + swiperId.nextIdBtn,
           prevEl: '#' + swiperId.prevIdBtn,
         },
+      });
+
+      // CHANGE IMG ON HOVER
+      document.getElementById(swiperId.id).addEventListener('mouseenter', () => {
+        imgSwiper.slideNext(400, false)
+      })
+
+      document.getElementById(swiperId.id).addEventListener('mouseleave', () => {
+        imgSwiper.slideTo(0, 400, false)
+      })
+
+      //VISIBLE PAGINATION
+      const prevBtn = document.getElementById(swiperId.prevIdBtn);
+      prevBtn.style.opacity = 0;
+      prevBtn.style.pointerEvents = 'none';
+      prevBtn.style.transition = 'opacity 0.3s ease-in-out';
+
+      const nextBtn = document.getElementById(swiperId.nextIdBtn);
+      nextBtn.style.opacity = 0;
+      nextBtn.style.pointerEvents = 'none';
+      nextBtn.style.transition = 'opacity 0.3s ease-in-out';
+
+      productSlide.addEventListener('mouseover', () => {
+        prevBtn.style.opacity = 1;
+        prevBtn.style.pointerEvents = 'auto';
+        nextBtn.style.opacity = 1;
+        nextBtn.style.pointerEvents = 'auto';
+      })
+      productSlide.addEventListener('mouseout', () => {
+        prevBtn.style.opacity = 0;
+        prevBtn.style.pointerEvents = 'none';
+        nextBtn.style.opacity = 0;
+        nextBtn.style.pointerEvents = 'none';
       })
     }
   })
 
   // TOP PRODUCT SWIPER INIT
   const topProductSwiper = new Swiper('.top-products__list', {
-    slidesPerView: 4,
+    slidesPerView: 5,
     spaceBetween: 0,
     allowTouchMove: false,
     loop: true,
